22ec76fa1fa4c6cbe0df6039dfb77c176056915a
ryanpahlevi <ryanpahlevi3@gmail.com>
2015-10-14 14:39:07 +0700

    14311827_AhmadYurianPahlevi_SI14E_Tugas2
    TUGAS2

---
+++ tugas2/src/tugas2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author acerOne14
+ */
+public class tugas2 {
+    public int nilaiHasil;
+    
+    int penjumlahan(int a, int b){
+        nilaiHasil=a+b;
+        return nilaiHasil;
+    }
+    int pengurangan(int a, int b){
+        nilaiHasil=a-b;
+        return nilaiHasil;
+    }
+    double pembagian(int a, int b){
+        nilaiHasil=a/b;
+        return nilaiHasil;
+    }
+    int perkalian(int a, int b){
+        nilaiHasil=a*b;
+        return nilaiHasil;
+    }
+}
